8e75b63d966d6a1f4950a15fa479fe725c78c5e8
grace stratton <35736264+spaceygrace@users.noreply.github.com>
2023-11-08 20:38:11 -0400

    react fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from "./routes/Home"
+import Rsvp from './routes/Rsvp';
+// import demo
 
 function App() {
   return (
+
     <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        {/* <Route path="/demo" element={ <Home /> } /> */}
+        <Route path="/rsvp" element={ <Rsvp /> } />
+      </Routes>
+
+
       <header className="App-header">
         <p>Coming Soon</p>
       </header>
